c1a49970fd179935892741231ec24148f480c2f4
Alex M. <you@example.com>
2026-08-04 19:31:13 +0300

    update server config with real data

--- nixos/phase1-installable-base/site.nix
+++ nixos/phase1-installable-base/site.nix
@@ -1,23 +1,23 @@
 {
   system = "x86_64-linux";
   hostName = "personal-assistant-1";
-  timeZone = "Europe/Amsterdam";
+  timeZone = "Etc/UTC";
   locale = "en_US.UTF-8";
 
   bootMode = "bios"; # or "efi"
-  disk = "/dev/vda";
+  disk = "/dev/sda";
 
-  publicIPv4 = "REPLACE_ME_SERVER_IP";
+  publicIPv4 = "169.58.116.31";
 
   deploy = {
     sshUser = "root";
-    sshHost = "REPLACE_ME_SERVER_IP";
-    sshTarget = "root@REPLACE_ME_SERVER_IP";
+    sshHost = "169.58.116.31";
+    sshTarget = "root@169.58.116.31";
     sshIdentityFile = "/home/admin/.ssh/id_ed25519";
   };
 
   rootAuthorizedKeys = [
-    "REPLACE_ME_SSH_PUBLIC_KEY"
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILtxITw5sdJKdp5x+uzAcXVHHFn74yBREgZCazjm0mOC"
   ];
 
   network = {
@@ -25,10 +25,10 @@
     # parameters are copied here. Set useDHCP = false and replace every
     # placeholder below before selecting static networking.
     useDHCP = true;
-    interface = "REPLACE_ME_INTERFACE";
-    address = "REPLACE_ME_SERVER_IP";
+    interface = "eth0";
+    address = "169.58.116.31";
     prefixLength = 24;
-    gateway = "REPLACE_ME_GATEWAY";
+    gateway = "169.58.0.1";
     nameservers = [ "1.1.1.1" "8.8.8.8" ];
   };
 }
--- nixos/phase2-ops-base/site.nix
+++ nixos/phase2-ops-base/site.nix
@@ -1,34 +1,34 @@
 {
   system = "x86_64-linux";
   hostName = "personal-assistant-1";
-  timeZone = "Europe/Amsterdam";
+  timeZone = "Etc/UTC";
   locale = "en_US.UTF-8";
 
   bootMode = "bios"; # or "efi"
-  disk = "/dev/vda";
+  disk = "/dev/sda";
 
-  publicIPv4 = "REPLACE_ME_SERVER_IP";
+  publicIPv4 = "169.58.116.31";
 
   deploy = {
     sshUser = "admin";
-    sshHost = "REPLACE_ME_SERVER_IP";
-    sshTarget = "admin@REPLACE_ME_SERVER_IP";
+    sshHost = "169.58.116.31";
+    sshTarget = "admin@169.58.116.31";
     sshIdentityFile = "/home/admin/.ssh/id_ed25519";
   };
 
   adminUser = "admin";
 
   adminAuthorizedKeys = [
-    "REPLACE_ME_SSH_PUBLIC_KEY"
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILtxITw5sdJKdp5x+uzAcXVHHFn74yBREgZCazjm0mOC"
   ];
 
   network = {
     # Keep this in sync with phase1-installable-base/site.nix.
     useDHCP = true;
-    interface = "REPLACE_ME_INTERFACE";
-    address = "REPLACE_ME_SERVER_IP";
+    interface = "eth0";
+    address = "169.58.116.31";
     prefixLength = 24;
-    gateway = "REPLACE_ME_GATEWAY";
+    gateway = "169.58.0.1";
     nameservers = [ "1.1.1.1" "8.8.8.8" ];
   };
 }
